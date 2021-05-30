d02cf6a5db038c46fc16d540408bc725a4ea685d
gopi janardhan reddy Battula <74000735+JanardhanGopi@users.noreply.github.com>
2021-05-30 12:10:09 +0530

    Delete Part_2 directory

--- Part_2/Phonebook/components/Person.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Person = ({name, number, deletePerson}) => {
-  return (
-    <li>{name} {number} <button onClick={deletePerson}>delete</button></li>
-  )
-}
-
-export default Person;
